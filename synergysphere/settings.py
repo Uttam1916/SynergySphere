8d4d835fa41cb23c55701890229b4682d4102fa4
Uttam1916 <tired3120@gmail.com>
2025-09-06 09:08:30 +0530

    fixed naming

---
+++ synergysphere/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "synergysphere",
+        "USER": "synergyuser",
+        "PASSWORD": "password123",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
